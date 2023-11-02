99d19a2e4e73cef3e940cf123895156070cf0e94
Recai Oktaş <roktas@gmail.com>
2023-11-03 01:40:53 +0300

    Reorganise tests

---
+++ test/unit/basics_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative "../test_helper"
+
+class BasicsTest < Minitest::Test
+  FIXTURES = File.expand_path(File.join(__dir__, "..", "integration"))
+
+  def test_solve
+    request  = File.read(File.join(FIXTURES, "01-bafra-in.json"))
+    expected = File.read(File.join(FIXTURES, "01-bafra-out.json"))
+
+    assert_equal(expected, Motor.solve(request))
+  end
+
+  def test_validate
+    assert_raises(Motor::InvalidData, "Data empty") { Motor.validate("{}") }
+  end
+end
