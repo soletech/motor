69162b8dd4b1490086f5338dfb24c1434705debd
Recai Oktaş <roktas@gmail.com>
2024-05-07 16:09:00 +0300

    Fix query

--- lib/motor/model.rb
+++ lib/motor/model.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
+require_relative "model/query"
+
 require_relative "model/problem"
 require_relative "model/solution"
--- lib/motor/model/problem.rb
+++ lib/motor/model/problem.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative "problem/problem"
-require_relative "problem/query"
 
 require_relative "problem/objective"
 require_relative "problem/constraints"
