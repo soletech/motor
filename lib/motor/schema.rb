d654e3a94c58b466991f50d9541747664f52353d
Recai Oktaş <roktas@gmail.com>
2024-12-26 19:12:25 +0300

    .

--- lib/motor/schema.rb
+++ lib/motor/schema.rb
@@ -4,64 +4,50 @@ require "dry/schema"
 
 module Motor
   module Schema
-    Objective = Dry::Schema.Params do
-      optional(:name).maybe(:string)
-      optional(:method).filled(:str?, included_in?: %w[ maximize minimize ])
-      required(:variables).array(:string)
-      required(:coefficients).array(:float)
-    end
+    Root = Dry::Schema.Params do # rubocop:disable Metrics/BlockLength
+      required(:objective).hash do
+        optional(:name).maybe(:string)
+        optional(:method).filled(:str?, included_in?: %w[ maximize minimize ])
+        required(:variables).array(:string)
+        required(:coefficients).array(:float)
+      end
+      required(:constraints).array(:hash) do
+        required(:constraint).filled(:string)
+        required(:rhs).filled(:float)
+        required(:relation).filled(:str?, included_in?: %w[ > >= = < <= ])
+        required(:coefficients).array(:float)
 
-    Constraint = Dry::Schema.Params do
-      required(:constraint).filled(:string)
-      required(:rhs).filled(:float)
-      required(:relation).filled(:str?, included_in?: %w[ > >= = < <= ])
-      required(:coefficients).array(:float)
-
-      before(:value_coercer) do |result|
-        result.to_h.map do |h|
-          h[:relation] = "=" if h[:relation] == "=="
+        before(:value_coercer) do |result|
+          result.to_h.map do |h|
+            h[:relation] = "=" if h[:relation] == "=="
+          end
         end
       end
-    end
-
-    module Solution
-      Result = Dry::Schema.Params do
-        optional(:code).filled(:string)
-        optional(:description).filled(:string)
-        required(:value).filled(:float)
-      end
-
-      module Sensitivity
-        Coefficient = Dry::Schema.Params do
-          required(:variable).filled(:string)
-          required(:value).filled(:float)
-          required(:reduced_cost).filled(:float)
-          required(:original_value).filled(:float)
-          required(:lower_bound).filled(:float)
-          required(:upper_bound).filled(:float)
-          required(:is_basic_variable).filled(:bool)
-        end
-
-        Boundary = Dry::Schema.Params do
-          required(:constraint).filled(:string)
-          required(:shadow_price).filled(:float)
-          required(:slack_or_surplus).filled(:float)
-          required(:original_value).filled(:float)
-          required(:lower_bound).filled(:float)
-          required(:upper_bound).filled(:float)
-          required(:neither_bounds_are_binding).filled(:bool)
-        end
-      end
-    end
-
-    Root = Dry::Schema.Params do
-      required(:objective).hash(Objective)
-      required(:constraints).array(Constraint)
       optional(:solution).hash do
-        required(:result).hash(Solution::Result)
+        required(:result).hash do
+          optional(:code).filled(:string)
+          optional(:description).filled(:string)
+          required(:value).filled(:float)
+        end
         optional(:sensitivity).hash do
-          required(:coefficients).array(Solution::Sensitivity::Coefficient)
-          required(:boundaries).array(Solution::Sensitivity::Boundary)
+          required(:coefficients).array(:hash) do
+            required(:variable).filled(:string)
+            required(:value).filled(:float)
+            required(:reduced_cost).filled(:float)
+            required(:original_value).filled(:float)
+            required(:lower_bound).filled(:float)
+            required(:upper_bound).filled(:float)
+            required(:is_basic_variable).filled(:bool)
+          end
+          required(:boundaries).array(:hash) do
+            required(:constraint).filled(:string)
+            required(:shadow_price).filled(:float)
+            required(:slack_or_surplus).filled(:float)
+            required(:original_value).filled(:float)
+            required(:lower_bound).filled(:float)
+            required(:upper_bound).filled(:float)
+            required(:neither_bounds_are_binding).filled(:bool)
+          end
         end
       end
     end
